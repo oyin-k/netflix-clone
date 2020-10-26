282f9e0258ba77700f86485850b6b75c66cf20d4
oyin-k <oyink7@gmail.com>
2020-10-26 04:09:02 +0100

    routes

---
+++ src/pages/signin.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Signin() {
+  return <p>I am Signin</p>;
+}
---
+++ src/pages/signup.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Signup() {
+  return <p>I am Signup</p>;
+}
